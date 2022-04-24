13f5ff8639b73c9eb18e02509383983e3be99b82
Tegar Cahya Bayu Siregar <64503005+MrCoeg@users.noreply.github.com>
2022-04-24 14:12:40 +0700

    Update AnyaForger.java

--- src/main/java/code/league/april/AnyaForger.java
+++ src/main/java/code/league/april/AnyaForger.java
@@ -2,36 +2,19 @@ package code.league.april;
 
 public class AnyaForger {
     public static Integer solution(Integer value) {
-        int kejadian = 0;
-
-        switch (value) {
-            case 2:
-                kejadian += 1;
-            case 3:
-                kejadian += 2;
-            case 4:
-                kejadian += 3;
-            case 5:
-                kejadian += 4;
-            case 6:
-                kejadian += 5;
-            case 7:
-                kejadian += 6;
-            case 8:
-                kejadian += 5;
-            case 9:
-                kejadian += 4;
-            case 10:
-                kejadian += 3;
-            case 11:
-                kejadian += 2;
-            case 12:
-                kejadian += 1;
-                break;
-            default:
-                return 0;
+        if (value < 2 || value > 12){
+            return 0;
         }
+        int kejadian = 36;
+        for(int i = 1; i < value-1; i++){
 
+            if(i > 6){
+                kejadian -= 6 - (i%6);
+            }else{
+                kejadian -= i;
+            }
+
+        }
         return kejadian;
     }
 }
