2f6a21132c4da3ef5b97f620b79d739874f8c88c
dzakyy04 <dzakylinggau@gmail.com>
2022-06-24 22:45:34 +0700

    Add Gobta dan Gobzo

---
+++ src/main/java/code/league/juni/GobtaDanGobzo.java
@@ -0,0 +1,23 @@
+package code.league.juni;
+
+public class GobtaDanGobzo {
+	public static String solution(int n) {
+		boolean ans = true;
+		while(n > 0) {
+			if(n % 5 == 2 || n % 5 == 3) {
+				ans = false;
+				break;
+			} else if(n % 5 == 4) {
+				n = (n+1) / 5;
+			} else {
+				n /= 5;
+			}
+		}
+		
+		if(ans == true) {
+			return "DONE GAN";
+		} else {
+			return "UP GAN";
+		}
+	}
+}
